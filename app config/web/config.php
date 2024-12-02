8c4d0d3d6dc24ce07bc87d80b14ea8087dc8d2df
Soufyane Youbi <soufyane52@gmail.com>
2024-12-02 19:12:28 +0100

    Reaorganizacion y minikube mysql
    Reaorganizacion y minikube mysql

---
+++ app config/web/config.php
@@ -0,0 +1,11 @@
+<?php
+// config.php
+
+return [
+    'db2' => [
+        'host' => 'mysql_db_pro',
+        'user' => 'root',
+        'password' => 'pro-pass',
+        'dbname' => 'pro.db',
+    ]
+];
