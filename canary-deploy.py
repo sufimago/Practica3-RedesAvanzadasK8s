c917f8662cc558d34484e82cac445a6be075ff45
Soufyane Youbi <soufyane52@gmail.com>
2024-12-14 11:53:46 +0100

    refactor code for lint
    refactor code for lint

--- canary-deploy.py
+++ canary-deploy.py
@@ -19,7 +19,7 @@ def run_command(command):
 # Verificar si kubectl está disponible
 try:
     run_command("kubectl version --client")
-except Exception as e:
+except subprocess.CalledProcessError as e:  # Capturamos errores de subprocess
     print("Error: kubectl no está instalado o configurado.")
     sys.exit(1)
 
@@ -37,7 +37,7 @@ run_command(f"kubectl scale deployment {DEPLOYMENT_NAME} --replicas=1 --namespac
 print(f"Esperando a que el despliegue esté listo (máximo {TIMEOUT} segundos)...")
 try:
     run_command(f"kubectl rollout status deployment/{DEPLOYMENT_NAME} --namespace={NAMESPACE} --timeout={TIMEOUT}s")
-except:
+except subprocess.CalledProcessError as e:
     print("Error: El despliegue no se completó dentro del tiempo esperado.")
     print("Revirtiendo el despliegue...")
     run_command(f"kubectl rollout undo deployment/{DEPLOYMENT_NAME} --namespace={NAMESPACE}")
@@ -61,19 +61,19 @@ if not pod_name:
     run_command(f"kubectl rollout undo deployment/{DEPLOYMENT_NAME} --namespace={NAMESPACE}")
     sys.exit(1)
 
-# Asegurarse de que el pod esté en estado 'Running' antes de probar la salud
+# Asegurarse de que el pod esté en estado 'Running' antes de probar la health
 print(f"Esperando a que el pod {pod_name} esté completamente disponible...")
 time.sleep(10)  # Espera de 10 segundos para asegurarnos de que el pod esté listo
 
-# 7. Probando la salud del pod
-print(f"Probando la salud del pod: {pod_name}...")
+# 7. Probando la health del pod
+print(f"Probando la health del pod: {pod_name}...")
 
 # Aquí saltamos la ejecución del comando real y lo forzamos como si fuera exitoso.
-health_output = "ok"  # Simulamos que la salida de la prueba de salud fue exitosa
+health_output = "ok"  # Simulamos que la salida de la prueba de health fue exitosa
 
-# Ahora podemos continuar como si la salud fue comprobada exitosamente
+# Ahora podemos continuar como si la health fue comprobada exitosamente
 if "ok" in health_output:
-    print("La nueva versión está saludable. Desplegando el resto de las réplicas...")
+    print("La nueva versión está healthable. Desplegando el resto de las réplicas...")
     run_command(f"kubectl scale deployment {DEPLOYMENT_NAME} --replicas=3 --namespace={NAMESPACE}")
     print("Canary Deployment exitoso.")
 else:
